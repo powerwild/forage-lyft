43f03f9e89d0094c6c59bf667f9c9f1149f27778
Casey Spears <spearscase@gmail.com>
2023-01-27 17:02:30 -0600

    added abs method on battery class

--- battery/battery.py
+++ battery/battery.py
@@ -1,7 +1,11 @@
 from abc import ABC, abstractmethod
 
 class Battery(ABC):
+
     @abstractmethod
     def needs_service(self):
         pass
-    
+
+    @abstractmethod
+    def perform_service(self):
+        pass
